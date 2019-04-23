b807d49ddfedf233b52177e32dddb58f71322264
Robert Jackson Jr <robertjacksonj2@gmail.com>
2019-04-23 00:10:00 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Robert Jackson Jr on 4/22/19.
+//  Copyright © 2019 Robert Jackson Jr. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+}
